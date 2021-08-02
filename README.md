fabd5336906ba2166b66dee5e7b600dcade9d959
mcnuttandrew <mcnutt.andrew@gmail.com>
2021-08-02 22:30:15 +0000

    Deploying to gh-pages from @ mcnuttandrew/zine-potential@807eb5dc7086d626d4fe6ea73e737b495488bf2c 🚀

---
+++ README.md
@@ -0,0 +1,18 @@
+# On the Potential of Zines as Medium for Visualization - Supplement Site
+
+Install the dependencies...
+
+```bash
+cd svelte-app
+npm install
+```
+
+...then start [Rollup](https://rollupjs.org):
+
+```bash
+npm run dev
+```
+
+
+
+
---
+++ src/App.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+</script>
+
+<main>
+  <h1>On the Potential of Zines as Medium for Visualization</h1>
+  <h3>By <a href="http://mcnutt.in/">Andrew McNutt</a></h3>
+
+  <div class="flex">
+    <a href="assets/printable-zine.pdf" target="_blank">
+      <img src="assets/zine-cover.png" alt="cover of the zine describing the paper" />
+    </a>
+    <div class="description">
+      The world of zines is a big place! There are likely a bunch more zines out there that feature
+      visualization or maps or charts. If there are other zines featuring visualizations that you'd like to be
+      featured here, please let me know either via a <a
+        href="https://github.com/mcnuttandrew/zine-potential/issues/new/choose">github issue</a
+      > or drop me an email at mcnutt at uchicago dot edu.
+    </div>
+  </div>
+</main>
+
+<style>
+  main {
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    /* color: #ff3e00; */
+    /* text-transform: uppercase; */
+    font-size: 4em;
+  }
+
+  .flex {
+    display: flex;
+  }
+
+  .flex-down {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .description {
+    padding: 10px;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: 600px;
+    }
+  }
+</style>
