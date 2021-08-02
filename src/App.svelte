807eb5dc7086d626d4fe6ea73e737b495488bf2c
Andrew Michael McNutt <mcnutt.andrew@gmail.com>
2021-08-02 17:29:46 -0500

    add

--- src/App.svelte
+++ src/App.svelte
@@ -4,7 +4,19 @@
 <main>
   <h1>On the Potential of Zines as Medium for Visualization</h1>
   <h3>By <a href="http://mcnutt.in/">Andrew McNutt</a></h3>
-  <div>UGH STOP</div>
+
+  <div class="flex">
+    <a href="assets/printable-zine.pdf" target="_blank">
+      <img src="assets/zine-cover.png" alt="cover of the zine describing the paper" />
+    </a>
+    <div class="description">
+      The world of zines is a big place! There are likely a bunch more zines out there that feature
+      visualization or maps or charts. If there are other zines featuring visualizations that you'd like to be
+      featured here, please let me know either via a <a
+        href="https://github.com/mcnuttandrew/zine-potential/issues/new/choose">github issue</a
+      > or drop me an email at mcnutt at uchicago dot edu.
+    </div>
+  </div>
 </main>
 
 <style>
@@ -18,12 +30,24 @@
     /* color: #ff3e00; */
     /* text-transform: uppercase; */
     font-size: 4em;
-    font-weight: 100;
+  }
+
+  .flex {
+    display: flex;
+  }
+
+  .flex-down {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .description {
+    padding: 10px;
   }
 
   @media (min-width: 640px) {
     main {
-      max-width: none;
+      max-width: 600px;
     }
   }
 </style>
